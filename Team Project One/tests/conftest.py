ee7abdbb4fb9bfee57a3023acbc1561fc78f2c79
Jtr28 <jtrichardsonTRI@gmail.com>
2025-05-08 10:01:55 -0500

    Fix import path for pytest app fixture

--- Team Project One/tests/conftest.py
+++ Team Project One/tests/conftest.py
@@ -1,11 +1,12 @@
+import pytest
 import sys
 import os
-import pytest
 
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from app import app
+
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
